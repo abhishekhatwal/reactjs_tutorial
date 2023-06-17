db2baedecbc4c42de2b1649ada8d03fa9745d294
abhishekhatwal <abhishekhatwal18@gmail.com>
2023-06-17 11:06:18 +0530

    attributes in jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// attributes of html should be camelcase without any hyphen(-) is written in jsx
+//all element of html which have no closing tag should have closing tag in jsx e.g-img,br,hr tag should have closing tag in jsx 
 
-//displaying current date and time in dd/mm/yyyy and h/m/s
-const name = "abhi";
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+const name = "vinod";
+
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+
 ReactDOM.render(
-<>
-<h1>Hello, My name is {name}</h1>
-<p>current date is ={currDate} </p>
-<p>current Time is ={currTime} </p>
+<> 
+<h1 contentEditable="true"> My name is {name} </h1>
+<img src={img1} alt="randomImages" />
+<img src={img2} alt="randomImages" />
+<img src={img3} alt="randomImages" />
 </>,
 document.getElementById("root")
-    
 );
