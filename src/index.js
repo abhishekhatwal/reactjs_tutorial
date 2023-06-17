fa70c584ceebf2d1ceedac76f384520c405ceba0
abhishekhatwal <abhishekhatwal18@gmail.com>
2023-06-17 11:56:46 +0530

    internal css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 
-//check index.css 
-//class word is reserve in js so className is used also for is reserverd word in js
-//external css working is same
+
+
+//for inline css rules-
+//1-attribute should be written in camelcase
+//2-vlaue should be enclosed in single court or double court
+//3-semicolon replace by comma seperated vlue
+//4-object should be pass in style and enclosed in {} bcz of js object is being used in jsx with {} tag
+
+
+const obj={
+    textAlign: 'center',
+    fontWeight: 'bolder'
+}
+
+const obj2={
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: '10px',
+    border: '2px solid black',
+    textAlign: 'center',
+    justifyContent: 'center'
+}
+
 
 const name = "abhi";
 
@@ -14,8 +33,8 @@ const img3 = "https://picsum.photos/300/300";
 
 ReactDOM.render(
 <> 
-<h1 className="heading"> My name is {name} </h1>
-<div className="main">
+<h1 style={obj}> My name is {name} </h1>
+<div style={obj2}>
 <img src={img1} alt="randomImages" />
 <img src={img2} alt="randomImages" />
 <img src={img3} alt="randomImages" />
