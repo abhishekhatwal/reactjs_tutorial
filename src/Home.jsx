e6985c54bc66920b891b569f1f537cef8ca4ed29
abhishekhatwal <abhishekhatwal18@gmail.com>
2023-06-24 14:10:13 +0530

    useLocation hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,16 @@
 // Home.js
+//demostration of useLocation() router hook
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+
+  const loc=useLocation();                    //give u object which contain current url info along with other information
+  console.log(loc)
+        
   return (
     <div>
-      <h1>Welcome to the Home page!</h1>
+      <h1>Welcome to the Home page! current path is {loc.pathname}</h1>
       <p>This is the landing page of our website.</p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -10,40 +10,6 @@ ReactDOM.render(<BrowserRouter><App/></BrowserRouter>,document.getElementById("r
 
 
 
+//aside from react hooks(useEffect,useState) router pakage also provide 3 hooks useParam,useLocation and useHistory
 
-
-//react router package allow u to create multipage website
-/*with help of anchor tag(href property) it is inconvenient to create multi page webiste bcz with anchor tag whole website reloads to get a new page but in react we 
- use link or Navlink router tag to create multipage website which only replace the component which we want to show instead of reloading whole website to show new page.
--->switch tag(now Routes tag) in router pacakge allow u to display seperate component in different page and route(different from Routes tag) tag is used give a path to components.
--->browserRouter,NavLink(Link),switch(Routes) and route are tags of router package*/
-
-//in order to use router package first import it using npm
-//then import { BrowserRouter } from "react-router-dom"; and encapsulate all component which are define in react project inside browserRouter tag.
-//import{Route,Switch(or Routes)} from "react-router-dom"; in which u define path
-//define path for each component using switch(or Routes) and route tag
-//use NavLink tag and provide the path(define above for components) of the component which we want to see in new webpage.
-
-
-
-//--------------------------------------------------working--------------------------------------------------------------
-/*
-App.js
-
-The App component is wrapped with the Router component(<BrowserRouter>) from react-router-dom, which enables the routing functionality.
-The Navbar component is rendered within the Router component(<BrowserRouter>), ensuring that the navigation bar is present on all pages.
-The Switch/Routes component from react-router-dom is used to render only the first Route that matches the current URL, providing the correct content based on the URL path.
-Inside the Switch/Routes component, there are multiple Route components, each representing a specific path and the component to be rendered when that path is matched.
-The exact prop is used on the root Route to ensure it matches only when the URL is an exact match.
-The component/element prop is used to specify the component to render when the corresponding path is matched.
-
-Navbar.js
-
-The Navbar component contains a list of Link components, which are provided by the react-router-dom library.
-Each Link component represents a navigation link in the navigation bar.
-The to prop of the Link component specifies the path to navigate to when the link is clicked.
-Clicking on a Link triggers a navigation event and updates the URL, causing the corresponding component defined in App.js to be rendered.
-Overall, the Router component in App.js handles the routing functionality by determining which component to render based on the URL path. The Link components in Navbar.js provide a way to navigate between different pages of the website by updating the URL and triggering the rendering of the appropriate component.
-
-
-*/
+//useLocation() hook give u the object which contain user current url
