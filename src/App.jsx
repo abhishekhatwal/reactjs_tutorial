6cef7517e31828458096416337809b02234fac81
abhishekhatwal <abhishekhatwal18@gmail.com>
2023-06-29 17:51:59 +0530

    useReducer hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react'
-import { useRef } from 'react';
+import React, { useReducer } from 'react'
+
+let reducerfun=(state,action)=>{
+ switch(action.type){
+  case "increment":return state+1;
+  case "decrement":return state-1;
+  default:return state;
+ }
+}
 
 const App = () => {
   
-  const firstname=useRef(null);
-
+  let initialstate=0;
+  const[changestate,dispatch]=useReducer(reducerfun,initialstate);
   
-  let fun=(e)=>{
-   e.preventDefault();
-    console.log(firstname.current.value);
-  }
 
   return (
     <>
-    <form action="" onSubmit={fun}>
-      <input type="text" ref={firstname} />
-      <input type="submit" />
-    </form>
-  
+    <h1>{changestate}</h1>
+     <button onClick={()=>{dispatch({type:"increment"})}}>increment num</button>
+     <button onClick={()=>{dispatch({type:"decrement"})}}>decrement num</button>
+    
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -6,12 +6,4 @@ import App from "./App";
 
 ReactDOM.render(<App/>,document.getElementById("root"));    
 
-//useRef hook-it is used in uncontrolled component where dom is used to access the input field/element.
-//useState hook is used in control component where react manipulate input field dynamically using onchange method and useState hook.
-/*
-major difference in control(in which useState used) and uncontrol(in which useRef is used) component is that in control com data validation
-is dynamic i.e if u type one letter also it will check validation using useState hook bcz of onchange method run in input field which triger
-useState hook every time data change but in useRef whole data in input field is validated only after form submit method run(in case of control com also before form submit method u can access/validate input field data) not on state 
-change(bcz no onchange method) .inside onsubmit method data from input field is collected using onref hook(i.e ref attribute give reference of input field and 
-ref.current.value gives data in input field) 
-*/
+//useReducer hook-it is same as redux and useState hook which is used in state management.
